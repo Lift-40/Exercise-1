775442dea7307d72d32a0b45a9a916aade8d0c9a
Space <avn@471.no>
2017-01-18 12:52:47 +0100

    Create unsafeThreading.go

---
+++ unsafeThreading.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    . "fmt"
+    "runtime"
+    "time"
+)
+
+var notZero uint32
+
+func thread1() {
+	for i := 0; i < 1000000; i++ {
+		notZero += 1
+	}
+	Println("Thread 1:")
+	Println(notZero)
+}
+
+func thread2() {
+	for i := 0; i < 1000000; i++ {
+		notZero -= 1
+	}
+	Println("Thread 1:")
+	Println(notZero)
+}
+
+func main() {
+    runtime.GOMAXPROCS(runtime.NumCPU())
+	
+    go thread1()
+	go thread2()
+	
+    time.Sleep(100*time.Millisecond)
+	Println("Result: ")
+    Println(notZero)
+}
